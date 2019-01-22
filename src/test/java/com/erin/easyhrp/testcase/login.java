6e33fbc1db59e0763df2a414738cec8a152325f5
cheny1p1ng <841123402@qq.com>
2019-01-22 16:08:02 +0800

    1

--- src/test/java/com/erin/easyhrp/testcase/login.java
+++ src/test/java/com/erin/easyhrp/testcase/login.java
@@ -13,35 +13,42 @@ import com.erin.easyhrp.base.CaseBase;
 import com.erin.easyhrp.base.DriverBase;
 import com.erin.easyhrp.business.LoginPro;
 
-public class login extends CaseBase{
-public DriverBase driver;
-public LoginPro loginPro;
-static Logger logger = Logger.getLogger(login.class);//log4j引用
-@BeforeClass
-public void loginTest() {
-	this.driver = InitDriver("chrome");
-	loginPro = new LoginPro(driver);
-}
-@Test
-public void getLoginHome() {
-	driver.get("http://betterme.easyhrp.cn/doct-webManager/bms/");
-}
+public class login extends CaseBase {
+	public DriverBase driver;
+	public LoginPro loginPro;
+	static Logger logger = Logger.getLogger(login.class);// log4j引用
 
-@Test(dependsOnMethods= {"getLoginHome"})
-@Parameters({"userName","passWord"})
-public void login(String userName,String passWord) {
-	logger.debug("登录开始-------------------------");
-	loginPro.login(userName, passWord);
-}
-
-@AfterClass
-public void after() {
-	try {
-		Thread.sleep(2000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	@BeforeClass
+	public void loginTest() {
+		this.driver = InitDriver("chrome");
+		loginPro = new LoginPro(driver);
 	}
-	driver.stop();
-}
+
+	@Test
+	public void getLoginHome() {
+		driver.get("http://betterme.easyhrp.cn/doct-webManager/bms/");
+	}
+
+//@Test(dependsOnMethods= {"getLoginHome"})
+//@Parameters({"userName","passWord"})
+//public void login(String userName,String passWord) {
+//	logger.debug("登录开始-------------------------");
+//	loginPro.login(userName, passWord);
+//}
+	@Test(dependsOnMethods = { "getLoginHome" })
+	public void login() {
+		logger.debug("登录开始-------------------------");
+		loginPro.login("ywm", "123456");
+	}
+
+	@AfterClass
+	public void after() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.stop();
+	}
 }
